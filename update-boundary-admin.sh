6b907d7b3041f34d70f73135c4d127148b662dfa
jpmilcent <jpm@clapas.org>
2014-04-12 16:57:12 +0200

    Ajout de la prise en compte de la table de référence

--- update-boundary-admin.sh
+++ update-boundary-admin.sh
@@ -54,61 +54,82 @@ else
 	fi
 fi
 
-# Maintain up to date the .osm.pbf file
-if [ ! -f "${DIR_OSM}/france.osm.pbf" ] ; then
-	echo "Téléchargement du fichier PBF initial...";
-	wget http://download.geofabrik.de/europe/france-latest.osm.pbf -O "${DIR_OSM}/france.osm.pbf"
-else
-	# Check if an update has been made less than 8 hours
-	if [ `ageEnSeconde "${DIR_OSM}/france.osm.pbf"` -gt 28800 ] ; then
-		echo "Mise à jour du fichier PBF...";
-		mv ${DIR_OSM}/france.osm.pbf ${DIR_OSM}/france_old.osm.pbf
-		${DIR_BIN}/osmupdate -B=${DIR_POLY}/france.poly -v -t=${DIR_TMP}/osmupdate/temp --day ${DIR_OSM}/france_old.osm.pbf ${DIR_OSM}/france.osm.pbf
-		rm -f ${DIR_OSM}/france_old.osm.pbf
-	else
-		echo "france.osm.pbf up to date";
-	fi
-fi
+# Processing for each area
+AREAS=( "france" "africa" )
+for AREA in "${AREAS[@]}"
+do
+	AREA_TIME_START=$(date +%s)
 
-# Create boundary extract
-if [ ! -f "${DIR_OSM}/france.osm.pbf" ] ; then
-	echo "Impossible de trouver france.osm.pbf.";
-	exit 1;
-else
-	if [ ! -f "${DIR_OSM}/france_boundary.osm.pbf" ] || [ `ageEnSeconde "${DIR_OSM}/france_boundary.osm.pbf"` -gt 28800 ] ; then
-		echo "Filtrage des zones administratives en cours..."
-		${DIR_BIN}/osmconvert ${DIR_OSM}/france.osm.pbf --out-o5m > ${DIR_OSM}/france.o5m
-		${DIR_BIN}/osmfilter -t=${DIR_TMP}/osmfilter_tempfile \
-			${DIR_OSM}/france.o5m \
-			--keep= \
-			--keep-nodes= \
-			--keep-ways= \
-			--keep-relations="type=boundary and boundary=administrative and admin_level=*" \
-			--out-o5m | ${DIR_BIN}/osmconvert - --out-pbf -o=${DIR_OSM}/france_boundary.osm.pbf
-		rm -f ${DIR_OSM}/france.o5m
+	# Maintain up to date the .osm.pbf file
+	if [ ! -f "${DIR_OSM}/${AREA}.osm.pbf" ] ; then
+		echo "Téléchargement du fichier PBF initial pour la zone «${AREA}»...";
+		if [ $AREA == "france" ] ; then
+			URL="http://download.geofabrik.de/europe/${AREA}-latest.osm.pbf"
+		else
+			URL="http://download.geofabrik.de/${AREA}-latest.osm.pbf"
+		fi
+		wget $URL -O "${DIR_OSM}/${AREA}.osm.pbf"
 	else
-		echo "france_boundary.osm.pbf up to date";
+		# Check if an update has been made less than 8 hours
+		if [ `ageEnSeconde "${DIR_OSM}/${AREA}.osm.pbf"` -gt 28800 ] ; then
+			echo "Mise à jour du fichier PBF pour la zone «${AREA}»...";
+			mv ${DIR_OSM}/${AREA}.osm.pbf ${DIR_OSM}/${AREA}_old.osm.pbf
+			${DIR_BIN}/osmupdate -B=${DIR_POLY}/${AREA}.poly -v -t=${DIR_TMP}/osmupdate/temp --day ${DIR_OSM}/${AREA}_old.osm.pbf ${DIR_OSM}/${AREA}.osm.pbf
+			rm -f ${DIR_OSM}/${AREA}_old.osm.pbf
+		else
+			echo "${AREA}.osm.pbf up to date";
+		fi
 	fi
-fi
 
-# Import into Mysql
-if [ ! -f "${DIR_OSM}/france_boundary.osm.pbf" ] ; then
-	echo "Impossible de trouver france_boundary.osm.pbf.";
-	exit 1;
-else
-	echo "Importation dans MySQL en cours ...";
-	${DIR_BIN_OGR}/ogr2ogr \
-		--config OSM_CONFIG_FILE $OSM_CONF_INI \
-		--config MYSQL_UNIX_PORT $MYSQL_UNIX_PORT \
-		-overwrite \
-		-progress \
-		-f "MySQL" MYSQL:${MYSQL_DATABASE},user=${MYSQL_USER},password=${MYSQL_PASSWORD},host=${MYSQL_HOST},port=${MYSQL_PORT} \
-		-lco engine=MYISAM \
-		-lco spatial_index=no \
-		"${DIR_OSM}/france_boundary.osm.pbf" multipolygons points
-fi
+	# Create boundary extract
+	if [ ! -f "${DIR_OSM}/${AREA}.osm.pbf" ] ; then
+		echo "Impossible de trouver le fichier ${AREA}.osm.pbf.";
+		exit 1;
+	else
+		if [ ! -f "${DIR_OSM}/${AREA}_boundary.osm.pbf" ] || [ `ageEnSeconde "${DIR_OSM}/${AREA}_boundary.osm.pbf"` -gt 28800 ] ; then
+			echo "Filtrage des zones administratives en cours pour la zone «${AREA}»..."
+			${DIR_BIN}/osmconvert ${DIR_OSM}/${AREA}.osm.pbf --out-o5m > ${DIR_OSM}/${AREA}.o5m
+			${DIR_BIN}/osmfilter -t=${DIR_TMP}/osmfilter_tempfile \
+				${DIR_OSM}/${AREA}.o5m \
+				--keep= \
+				--keep-nodes= \
+				--keep-ways= \
+				--keep-relations="type=boundary and boundary=administrative and admin_level=*" \
+				--out-o5m | ${DIR_BIN}/osmconvert - --out-pbf -o=${DIR_OSM}/${AREA}_boundary.osm.pbf
+			rm -f ${DIR_OSM}/${AREA}.o5m
+		else
+			echo "${AREA}_boundary.osm.pbf up to date";
+		fi
+	fi
+
+	# Import into Mysql
+	if [ ! -f "${DIR_OSM}/${AREA}_boundary.osm.pbf" ] ; then
+		echo "Impossible de trouver le fichier ${AREA}_boundary.osm.pbf.";
+		exit 1;
+	else
+		echo "Importation dans MySQL en cours pour la zone «${AREA}»...";
+		${DIR_BIN_OGR}/ogr2ogr \
+			--config OSM_CONFIG_FILE $OSM_CONF_INI \
+			--config MYSQL_UNIX_PORT $MYSQL_UNIX_PORT \
+			-overwrite \
+			-progress \
+			-f "MySQL" MYSQL:${MYSQL_DATABASE},user=${MYSQL_USER},password=${MYSQL_PASSWORD},host=${MYSQL_HOST},port=${MYSQL_PORT} \
+			-lco engine=MYISAM \
+			-lco spatial_index=no \
+			"${DIR_OSM}/${AREA}_boundary.osm.pbf" multipolygons points
+	fi
+
+	# Create and maintain the ref table
+	echo -e "Mise à jour de la table de référence pour la zone «${AREA}»..."
+	$BIN_PHP ./update-boundary-admin-ref.php ${AREA}
+
+	# Show time elapsed
+	AREA_TIME_END=$(date +%s)
+	AREA_TIME_DIFF=$(($AREA_TIME_END - $AREA_TIME_START));
+	echo "Time elapsed for area '${AREA}' : "`displaytime "$AREA_TIME_DIFF"`
+done
 
 # Show time elapsed
 TIME_END=$(date +%s)
 TIME_DIFF=$(($TIME_END - $TIME_START));
-echo "Time elapsed : "`displaytime "$TIME_DIFF"`
+echo "Total time elapsed : "`displaytime "$TIME_DIFF"`
