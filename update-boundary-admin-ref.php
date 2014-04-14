09779ee21c4b5bf9fbdfd171e20d18c7d788af03
jpmilcent <jpm@clapas.org>
2014-04-14 09:36:19 +0200

    Début prise en compte multi zones + compil opensuse

--- update-boundary-admin-ref.php
+++ update-boundary-admin-ref.php
@@ -16,6 +16,8 @@
  * @license    CeCILL v2 <http://www.cecill.info/licences/Licence_CeCILL_V2-fr.txt>
  * @license    GNU GPL <http://www.gnu.org/licenses/gpl.html>
  */
+$zone = $argv[1];
+
 define('DS', DIRECTORY_SEPARATOR);
 $config = parse_ini_file(__DIR__.DS.'config.cfg');
 $configOsm = parse_ini_file(__DIR__.DS.'osmconf-boundary.ini', true);
@@ -23,8 +25,8 @@ $configOsm = parse_ini_file(__DIR__.DS.'osmconf-boundary.ini', true);
 // Get params
 $laundering = $configOsm['attribute_name_laundering'] ? true : false;
 $fields = array(
-	'OGR_FID' => 'bigint(20) NOT NULL AUTO_INCREMENT',
-	'SHAPE' => 'geometry NOT NULL'
+	'ogr_fid' => 'bigint(20) NOT NULL AUTO_INCREMENT',
+	'shape' => 'geometry NOT NULL'
 );
 if ($configOsm['multipolygons']['osm_id'] == 'yes') {
 	$fields['osm_id'] = 'bigint(20) unsigned DEFAULT NULL';
@@ -48,7 +50,11 @@ foreach ($fields as $fieldName => $fieldFormat) {
 }
 $queriesInit[] = "CREATE TABLE IF NOT EXISTS multipolygons_ref (
   $fieldsInfos
-  UNIQUE KEY `OGR_FID` (`OGR_FID`),
+  date_add DATETIME,
+  date_update DATETIME,
+  date_vanish DATETIME,
+  zone varchar(50),
+  UNIQUE KEY `ogr_fid` (`ogr_fid`),
   KEY `osm_id` (`osm_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
 $queriesInit[] = "ALTER TABLE multipolygons CHANGE osm_id osm_id BIGINT NULL DEFAULT NULL ;";
@@ -76,17 +82,18 @@ try {
 
 // New multipolygons
 $query = 'INSERT INTO multipolygons_ref '.
-	'SELECT *, NOW(), NULL, NULL '.
+	"SELECT *, NOW(), NULL, NULL, $zone ".
 	'FROM multipolygons AS m '.
 	'WHERE m.osm_id IS NOT NULL '.
-	'AND m.osm_id NOT IN (SELECT osm_id FROM multipolygons_ref) ';
+	"AND m.osm_id NOT IN (SELECT osm_id FROM multipolygons_ref WHERE zone = '$zone' ) ";
 $rowsAddedNumber = $db->exec($query);
 echo "Nombre de multi-polygones ajoutés : $rowsAddedNumber\n";
 
 // Multipolygons vanished
 $query = 'UPDATE multipolygons_ref '.
 	'SET date_vanish = NOW() '.
-	'WHERE osm_id NOT IN (SELECT osm_id FROM multipolygons WHERE osm_id IS NOT NULL) ';
+	"WHERE zone = '$zone' ".
+	'AND osm_id NOT IN (SELECT osm_id FROM multipolygons WHERE osm_id IS NOT NULL ) ';
 $rowsVanishedNumber = $db->exec($query);
 echo "Nombre de multi-polygones ayant disparu : $rowsVanishedNumber\n";
 
@@ -100,7 +107,8 @@ $query = 'UPDATE multipolygons_ref AS mr '.
 	'	INNER JOIN multipolygons AS m ON (mr.osm_id = m.osm_id) '.
 	"SET $setClause,".
 	'	date_update = NOW() '.
-	'WHERE mr.osm_version < m.osm_version '.
+	"WHERE zone = '$zone' ".
+	'AND mr.osm_version < m.osm_version '.
 	'AND m.osm_id IS NOT NULL ';
 $rowsUpdatedNumber = $db->exec($query);
 echo "Nombre de multi-polygones modifiés : $rowsUpdatedNumber\n";
--- utils/os13-1_compile_gdal-1.10.sh
+++ utils/os13-1_compile_gdal-1.10.sh
@@ -7,45 +7,43 @@
 # Log des modifications de ce script :
 # 2014-04-11 [Jean-Pascal MILCENT] : Creation du script Gdal v 1.10
 
-# Indiquer la version de Gdal à compiler
-VERSION="svn-trunk-2014.04.11"
-URL_DOWNLOAD="http://www.gdal.org/daily/gdal-${VERSION}.tar.gz"
-BUILD_DIR=`pwd`"/../tmp"
-INSTALL_DIR=`pwd`"/../bin"
-MYSQL_CONFIG="/opt/lampp/bin/mysql_config"
-
-RCol='\e[0m';# Text Reset
-Gre='\e[0;32m'; # Text Green
+# Load config
+if [ -f ./../config.cfg ] ; then
+	source ./../config.cfg
+else
+	echo "${Red}Veuillez paramétrer le script en renommant le fichier 'config.defaut.cfg' en 'config.cfg'.${RCol}"
+	exit;
+fi
 
 echo -e "${Gre}Installation des paquets pour les librairies standards :${RCol}"
 sudo zypper in libxml2-devel libexpat-devel sqlite3-devel pcre-devel
 
 echo -e "${Gre}Création des dossiers pour la compilation${RCol}"
-if [ ! -d ${BUILD_DIR}/src ] ; then
-	mkdir ${BUILD_DIR}/src
+if [ ! -d ${GDAL_BUILD_DIR}/src ] ; then
+	mkdir ${GDAL_BUILD_DIR}/src
 fi
-if [ ! -d ${BUILD_DIR}/src/targz ] ; then
-	mkdir ${BUILD_DIR}/src/targz
+if [ ! -d ${GDAL_BUILD_DIR}/src/targz ] ; then
+	mkdir ${GDAL_BUILD_DIR}/src/targz
 fi
 
 echo -e "${Gre}Récupération des sources à compiler :${RCol}"
-cd ${BUILD_DIR}/src/targz
-if [ ! -f gdal-${VERSION}.tar.gz ] ; then
-	wget $URL_DOWNLOAD -O gdal-${VERSION}.tar.gz
+cd ${GDAL_BUILD_DIR}/src/targz
+if [ ! -f gdal-${GDAL_VERSION}.tar.gz ] ; then
+	wget $GDAL_URL_DOWNLOAD -O gdal-${GDAL_VERSION}.tar.gz
 fi
 
 echo -e "${Gre}Décompression des sources :${RCol}"
 # Effacement du dossier source pré-existant au cas ou
-cd ${BUILD_DIR}/src
-rm -rf gdal-${VERSION}
+cd ${GDAL_BUILD_DIR}/src
+rm -rf gdal-${GDAL_VERSION}
 # Décompression des sources
-tar xvfz targz/gdal-${VERSION}.tar.gz
+tar xvfz targz/gdal-${GDAL_VERSION}.tar.gz
 # Déplacement dans le dossier des sources pour compiler
-cd gdal-${VERSION}
+cd gdal-${GDAL_VERSION}
 
 echo -e "${Gre}Configuration, compilation et installation :${RCol}"
 ./configure \
-	--prefix=${INSTALL_DIR}/gdal-${VERSION} \
+	--prefix=${GDAL_INSTALL_DIR}/gdal-${GDAL_VERSION} \
 	--with-threads \
 	--with-ogr \
 	--with-geos \
@@ -62,15 +60,18 @@ echo -e "${Gre}Configuration, compilation et installation :${RCol}"
 	--with-sqlite3=yes \
 	--with-pcre \
 	--with-mysql=$MYSQL_CONFIG \
-	--with-fgdb=${BUILD_DIR}/src/FileGDB_API
+	--with-fgdb=${GDAL_BUILD_DIR}/src/FileGDB_API
 
 make
 make install
 
 echo -e "${Gre}Copie des lib FileGDB dans le dossier lib de gdal :${RCol}"
-cd ${INSTALL_DIR}/gdal-${VERSION}/lib64
-cp ${BUILD_DIR}/src/FileGDB_API/lib/libFileGDBAPI.so ./libFileGDBAPI.so
-cp ${BUILD_DIR}/src/FileGDB_API/lib/libfgdbunixrtl.so ./libfgdbunixrtl.so
+cd ${GDAL_INSTALL_DIR}/gdal-${GDAL_VERSION}/lib64
+cp ${GDAL_BUILD_DIR}/src/FileGDB_API/lib/libFileGDBAPI.so ./libFileGDBAPI.so
+cp ${GDAL_BUILD_DIR}/src/FileGDB_API/lib/libfgdbunixrtl.so ./libfgdbunixrtl.so
 
 echo -e "${Gre}Ajout de liens symboliques vers les lib manquantes :${RCol}"
 ln -s /usr/lib64/libstdc++.so.6 libstdc++.so.6
+
+echo -e "${Gre}Gdal est en version :${RCol}"
+cd ${GDAL_INSTALL_DIR}/gdal-${GDAL_VERSION}/bin/gdal-config --version
