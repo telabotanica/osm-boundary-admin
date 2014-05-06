147f1ac89cc7ba337ff3554486c5ccf1cbba0286
jpmilcent <jpm@tela-botanica.org>
2014-05-06 11:50:46 +0200

    Placement dans le dossier de l'utilisateur pour éviter les problèmes de droits.

--- utils/osm-cron.sh
+++ utils/osm-cron.sh
@@ -4,17 +4,20 @@
 #
 while true
 do
-    HEURE=$(date "+%H")
-    # If we are at 3AM, we launch the script
-    if [ $HEURE -eq 3 ] ; then
-        logger "osm::update : starting"
-        sudo -u username /home/username/bin/update-boundary-admin.sh 2>&1 > $FILE_LOG
-        logger "osm::update : stopping"
-        # Sleep 22h : to avoid the problem of changing the summer time and the delay of a few seconds added to each launch
-        sleep 22h
-    else
-        # Attempt every hour until 3 AM
-        logger "osm::update : attempt to launch $HEURE hours"
-        sleep 1h
-    fi
+	HEURE=$(date "+%H")
+	# If we are at 3AM, we launch the script
+	if [ $HEURE -eq 3 ] ; then
+		logger "osm::update : starting"
+		cd /home/username/
+		echo "Lancement depuis le dossier : "`pwd` > /home/username/logs/`date +"%F"`.log
+		sudo -u osmtela /home/username/bin/osm-boundary-admin/update-boundary-admin.sh >> /home/username/logs/`date +"%F"`.log 2>&1
+		chown username:username /home/username/logs/*.log
+		logger "osm::update : stopping"
+		# Sleep 22h : to avoid the problem of changing the summer time and the delay of a few seconds added to each launch
+		sleep 22h
+	else
+		# Attempt every hour until 3 AM
+		logger "osm::update : attempt to launch $HEURE hours"
+		sleep 1h
+	fi
 done
