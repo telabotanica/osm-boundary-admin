f9fc6b1d73982ddec47b318c42c48d4398d80f45
Mathias Chouet <mathias@tela-botanica.org>
2014-09-26 12:02:09 +0200

    Modification du script osm-cron avec variables de configuration Ajout de .project à .gitignore

--- utils/osm-cron.sh
+++ utils/osm-cron.sh
@@ -1,23 +1,56 @@
 #!/bin/bash
 #
+# - 2014-09-26 [Mathias CHOUET] : added configuration variables in script header
+# - 2014-05-26 [Jean-Pascal MILCENT] : add sending email functionality
 # - 2014-04-22 [Jean-Pascal Milcent] : created
 #
+ANSI2HTML_BIN="/usr/local/sbin/ansi2html.sh" # required - see http://www.pixelbeat.org/scripts/ansi2html.sh
+USERNAME="username"
+USERGROUP="usergroup"
+OSM_HOME="/home/username"
+SCRIPT_HOME="/home/username/bin/osm-boundary-admin"
+LOGS_FOLDER="${OSM_HOME}/logs"
+EMAIL_FROM="Root SERVERNAME <root@servername.net>"
+EMAIL_TO="recipient@domain.org"
+SENDMAIL_BIN="/usr/sbin/sendmail"
+
 while true
 do
-	HEURE=$(date "+%H")
-	# If we are at 3AM, we launch the script
-	if [ $HEURE -eq 3 ] ; then
-		logger "osm::update : starting"
-		cd /home/username/
-		echo "Lancement depuis le dossier : "`pwd` > /home/username/logs/`date +"%F"`.log
-		sudo -u osmtela /home/username/bin/osm-boundary-admin/update-boundary-admin.sh >> /home/username/logs/`date +"%F"`.log 2>&1
-		chown username:username /home/username/logs/*.log
-		logger "osm::update : stopping"
-		# Sleep 22h : to avoid the problem of changing the summer time and the delay of a few seconds added to each launch
-		sleep 22h
-	else
-		# Attempt every hour until 3 AM
-		logger "osm::update : attempt to launch $HEURE hours"
-		sleep 1h
-	fi
+    HEURE=$(date "+%H")
+    # If we are at 2 AM, we launch the script
+    if [ $HEURE -eq 2 ] ; then
+        DATE=`date +"%F"`
+        LOG="${LOGS_FOLDER}/${DATE}.log"
+        LOG_HTML="${LOGS_FOLDER}/${DATE}_html.log"
+
+        # Runs the OSM boundary admin update
+        logger "osm::update : starting"
+        cd ${OSM_HOME}
+        echo "Lancement depuis le dossier : "`pwd` > $LOG
+        sudo -u ${USERNAME} ${SCRIPT_HOME}/update-boundary-admin.sh >> $LOG 2>&1
+
+        # Managing the logs
+        cat $LOG | ${ANSI2HTML_BIN} > $LOG_HTML
+        chown ${USERNAME}:${USERGROUP} ${LOGS_FOLDER}/*.log
+     
+        # Send an email with the log content
+        (   
+            echo "From: ${EMAIL_FROM}";
+            echo "To: ${EMAIL_TO} ";
+            echo "Subject: OSM log - $DATE";
+     
+            echo "MIME-Version: 1.0";
+            echo "Content-Type: text/html; charset=UTF-8";
+            echo "Content-Disposition: inline";
+            cat $LOG_HTML
+        ) | ${SENDMAIL_BIN} -t
+        logger "osm::update : stopping"
+
+        # Sleep 12h : to avoid the problem of changing the summer time and the delay of a few seconds added to each launch
+        sleep 12h
+    else
+        # Attempt every hour until 2 AM
+        logger "osm::update : attempt to launch at $HEURE o'clock"
+        sleep 1h
+    fi  
 done
